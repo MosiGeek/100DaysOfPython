ccbcd20807856888ed51123662c0ebab0584198a
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-10-18 14:28:18 +0000

    if elif statements

--- functions.py
+++ functions.py
@@ -1,16 +1,31 @@
-# use functions if you wanna ddo things over and over
+# # use functions if you wanna ddo things over and over
 
-# def nameOfFunction(Input):
-#     #Action
-#     return Output
-
-def addOne(number):
-    number += 1
-    return number
+# # def nameOfFunction(Input):
+# #     #Action
+# #     return Output
 
 
-val = 0
+# def addOne(number):
+#     number += 1
+#     return number
 
-output = addOne(val)
 
-print(output)
+# val = 0
+
+# output = addOne(val)
+
+# kout = addOne(5.8)
+# print(output)
+# print(kout)
+
+
+def addOneAddTwo(numOne, numTwo):
+    result = numOne + 1
+
+    result += result + numTwo
+    # result = result + result + 2
+    return result
+
+
+out = addOneAddTwo(3, 4)
+print(out)
---
+++ ifStatements.py
@@ -0,0 +1,24 @@
+# if condition:
+#     Action
+
+# click = True
+# like = 1
+
+# if click == True:
+#     like += 1
+#     click = False
+
+# print(like)
+
+temp = 14
+
+themo = 15
+print(themo)
+
+if temp >= 15:
+    themo += 5
+
+elif temp <= 15:
+    themo -= 5
+
+print(themo)
