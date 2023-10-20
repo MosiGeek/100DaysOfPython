ce6ad6896a8db959bac4f105bb77a59d72d37179
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-10-20 13:26:25 +0000

    for loop

--- loops.py
+++ loops.py
@@ -1,1 +1,36 @@
-# loops
+# # loops
+
+# # starting with for loops
+
+# word = "Hello"
+# letters = []
+
+# for i in word:
+#     print(i)
+#     if i == "e":
+#         print("What a funny letter")
+#     letters.append(i)
+
+
+# print("**************************************************")
+# print(letters)
+# print("**************************************************")
+
+# print(letters[1:4])
+
+
+numbers = [1,2,3,4,5]
+odd = []
+even = []
+for j in numbers:
+    if j%2 == 0:
+        print(f"{j} is Even")
+        even.append(j)
+    else:
+        print(f"{j} is Odd")
+        odd.append(j)
+
+print("**************************************")
+
+print(f"odd {odd}")
+print(f"even {even}")
