9651b2ef9044f0e38ee2d09fcb755360009d673a
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-10-23 17:37:23 +0200

    commint message

--- breakncontinue.py
+++ breakncontinue.py
@@ -1,5 +1,18 @@
 # breaking and continuing in Loopslear
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+# alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-for letter in alphabet:
-    print(letter)
+# for letter in alphabet:
+#     print(letter)
+    
+participants = ["Jen","Shauna","Kyle","Tamy","Vaskr"] 
+
+position = 1
+
+for name in participants:
+    
+    if name == "Tamy":
+        print(position,name)
+        # print("About to break",position)
+        break
+    # print("about to Increament",position)
+    position += 1
