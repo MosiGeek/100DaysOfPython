2feaa6c800fa142c8061970e5a9f95137c7438d8
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-10-23 18:02:52 +0200

    added input to the madness

--- breakncontinue.py
+++ breakncontinue.py
@@ -8,11 +8,23 @@ participants = ["Jen","Shauna","Kyle","Tamy","Vaskr"]
 
 position = 1
 
-for name in participants:
+# for name in participants:
     
-    if name == "Tamy":
-        print(position,name)
-        # print("About to break",position)
+#     if name == "Tamy":
+#         print(position,name)
+#         # print("About to break",position)
+#         break
+#     # print("about to Increament",position)
+#     position += 1
+
+name = str(input("Enter name to search here: "))
+
+index = 1
+for cur_index in range(len(participants)):
+    if participants[cur_index] == name:
+        print(f"found {name} at position {index}")
         break
-    # print("about to Increament",position)
-    position += 1
+    else:
+        print("Not Found")
+        
+    index += 1    
