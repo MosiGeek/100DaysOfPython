0c2f5f0a9123ea8281144ab125866ca580c9c6c5
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-10-23 07:41:23 +0000

    while loop

--- while.py
+++ while.py
@@ -1,1 +1,20 @@
 # while loop
+
+# while (condition):
+#     Action
+
+counter = 1
+number = 0
+
+while counter < 100:
+    number += 1
+    print(number,end="\t")
+    
+    if number%2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+    counter += 1
+
+
