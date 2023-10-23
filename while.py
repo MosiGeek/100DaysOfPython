1e2f574b8ce27ec4e3529783a88d354a3172d6ce
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-10-23 08:00:42 +0000

    nothing to see here

--- while.py
+++ while.py
@@ -3,18 +3,28 @@
 # while (condition):
 #     Action
 
-counter = 1
-number = 0
+# counter = 1
+# number = 0
 
-while counter < 100:
-    number += 1
-    print(number,end="\t")
+# while (counter <= 100):
+#     number += 1
+#     print(number,end="\t")
     
-    if number%2 == 0:
-        print("Even")
-    else:
-        print("Odd")
+#     if number%2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
 
-    counter += 1
+#     counter += 1
 
 
+counter = 1
+Sum = 0
+
+while (counter<=10):
+    Sum = Sum + counter
+    print(counter)
+    counter+=1
+
+print("***************************************************")
+print(Sum)
