b57acd4d06b061ee78f80a85798d8a70228219e3
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-10-24 13:54:56 +0200

    Exercise One Done

--- Execise1.py
+++ Execise1.py
@@ -7,3 +7,7 @@ for pos in range(1,Length+1):
 # use step to direct the loop which way to count
 for inner in range(Length-1,0,-1):
     print(character*inner)
+
+# reversing a loop
+# for i in range(10,0,-1):
+#     print(i)
