c3ff7c746d25de3964940e9be769b8923f39113d
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-10-24 12:48:24 +0200

    using step to redirect the loop counting direction

--- Execise1.py
+++ Execise1.py
@@ -1,8 +1,9 @@
 Length = 5
-innerLength = -5
+character = "o"
 for pos in range(1,Length+1):
     #print(-pos)
-    print("#"*pos)
+    print(character*pos)
 
-for inner in range(innerLength,0):
-    print("#"*(inner*-1))
+# use step to direct the loop which way to count
+for inner in range(Length-1,0,-1):
+    print(character*inner)
