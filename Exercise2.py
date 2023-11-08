eba7ca9feec0dea0df0f6ed7768ae385f120cbab
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-11-08 10:13:40 +0200

    Completed Exercise 2 on dictionaries

--- Exercise2.py
+++ Exercise2.py
@@ -1,20 +1,27 @@
 # Exercise 2 lists and dictiionaries Exercise
 
+# Examplee a shoe company that is using a dictionary to keep count of shoes in stock
 
 
-BlackShoes = {
-    42:2,
-    41:3,
-    40:4,
-    39:1,
-    38:0
-}
+BlackShoes = {42: 2, 41: 3, 40: 4, 39: 1, 38: 0}
 
-while(True):
-    purchaseSize = int(input("Which shoe size would you like to buy? :\n"))
-    
-    if BlackShoes[purchaseSize] in BlackShoes:
-        BlackShoes[purchaseSize] -= 1
-    else:
-        print("nothing here")
+
+print(BlackShoes)
+
+while True:
+    purchaseSize = int(
+        input(
+            "Which shoe size would you like to buy? (Enter any -ve Number to Exit ]:\n"
+        )
+    )
+
+    # exit the loop with any nagative number 
+    if purchaseSize < 0:
         break
+
+    if BlackShoes[purchaseSize] <= 0:
+        print(f"Sorry Size {purchaseSize} out of Stock! \n")
+    else:
+        BlackShoes[purchaseSize] -= 1
+
+    print(BlackShoes)
