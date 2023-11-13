22dbef5d6e6f4e2ca680f8430072b7b31d8bc4d5
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-11-13 11:08:57 +0200

    input output

---
+++ inputoutput.py
@@ -0,0 +1,22 @@
+# I/O in python
+
+# Var = input("Message to the user")
+
+# name = input("Whats your name: \n")
+
+# print(name)
+
+# age = int(input("whats your age? :"))
+
+
+# print(str(age)+"1")
+
+listOfNums = []
+print(listOfNums)
+
+for i in range(5):
+    val = int(input("Gimme an interger: "))
+    listOfNums.append(val)
+
+for i in range(len(listOfNums)):
+    print(listOfNums[i])
