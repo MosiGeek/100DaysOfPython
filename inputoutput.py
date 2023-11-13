5d3b08ba493cfeb9eba90cddbf83256e3f0bcd35
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-11-13 17:14:15 +0200

    input and format string

--- inputoutput.py
+++ inputoutput.py
@@ -11,16 +11,18 @@
 
 # print(str(age)+"1")
 
+
 listOfNums = []
 avg = 0
 print(listOfNums)
 
 for i in range(5):
-    val = int(input("Gimme an interger: "))
+    val = int(input(f"Gimme an interger-{str(i)} :"))
     listOfNums.append(val)
 
 # average score 
 for i in range(len(listOfNums)):
     avg += listOfNums[i]
 
-print(round(avg/len(listOfNums)))
+print(f"Average of {avg} given {len(listOfNums)} Numbers is : {round(avg/len(listOfNums))}")
+
