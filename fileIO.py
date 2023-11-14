8fcdb6e399c35208fca83a694d0283797bbd2ba1
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-11-14 18:46:56 +0200

    The following changes were made to the fileIO.py script:
    * The code to write the list of cities to the file `vacationPlaces.txt` was commented out.
    * The code to read the first three lines of the file `vacationPlaces.txt` was commented out.
    * The code to append the list of African cities to the file `vacationPlaces.txt` was commented out.
    * A new function `load()` was added to the script. This function takes a file object as an argument and prints the contents of the file to the console.
    * The `load()` function was called with the `file` object, which is a reference to the `vacationPlaces.txt` file.
    
    The following changes were made to the `vacationPlaces.txt` file:
    
    * The list of African cities was added to the end of the file.

--- fileIO.py
+++ fileIO.py
@@ -5,11 +5,25 @@
 cities = ["Peth","London","New York", "Tokyo","Joburg"]
 
 
-vacationFile = open("vacationPlaces.txt","w")
+vacationFile = open("vacationPlaces.txt","w") #writing mode
 
 for city in cities:
     vacationFile.write(city+"\n")
     
-print("done")
+# print("done")
 
 vacationFile.close()
+
+textFile = open("vacationPlaces.txt","r") #reading mode
+
+for line in textFile:
+    print(line,end="")
+
+
+# theWholeFile = openDis.read()
+
+
+# print(theWholeFile)
+
+# vacationFile.close()
+
