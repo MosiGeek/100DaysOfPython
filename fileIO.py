ed4529be69ec1da133a09fc45c585f9193d8cbfd
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-11-15 10:27:04 +0200

    The following changes were made to the vacationPlaces.txt file:
    * The cities of Tokyo, Delhi, Shanghai, Sao Paulo, Mumbai, Dhaka, Mexico City, Cairo, Bangkok, and Karachi were removed.
    * The cities of Harare, Lusaka, Gaberone, and Windhoek were added.

--- fileIO.py
+++ fileIO.py
@@ -1,29 +1,75 @@
-# File = open("filename","r") # r, w, a, r+
-# File.close()
+# # File = open("filename","r") # r, w, a, r+
+# # File.close()
 
 
-cities = ["Peth","London","New York", "Tokyo","Joburg"]
+# cities = ["Peth","London","New York", "Tokyo","Joburg"]
 
 
-vacationFile = open("vacationPlaces.txt","w") #writing mode
+# vacationFile = open("vacationPlaces.txt","w") #writing mode
 
-for city in cities:
-    vacationFile.write(city+"\n")
+# for city in cities:
+#     vacationFile.write(city+"\n")
     
-# print("done")
-
-vacationFile.close()
-
-textFile = open("vacationPlaces.txt","r") #reading mode
-
-for line in textFile:
-    print(line,end="")
-
-
-# theWholeFile = openDis.read()
-
-
-# print(theWholeFile)
+# # print("done")
 
 # vacationFile.close()
 
+# textFile = open("vacationPlaces.txt","r") #reading mode
+
+# FstLine = textFile.readline()
+# print(FstLine)
+
+# ScndLine = textFile.readline()
+# print(ScndLine)
+
+# ThrdLine = textFile.readline()
+# print(ThrdLine)
+
+
+# for line in textFile:
+#     print(line,end="")
+
+# textFile.close()
+
+# # more cities to append at the end of the file  
+
+# africa = ["Harare", "Lusaka", "Gaberone","Windhoek"] 
+
+# apendText = open("vacationPlaces.txt","a") # apending more text at the end of the file
+
+# for a_city in africa:
+#     apendText.write(a_city+"\n")
+
+# apendText.close()
+
+# print("*********************************")
+
+
+# # readFinal = open("vacationPlaces.txt","r") #reading the file
+
+# # for text in readFinal:
+# #     print(text)
+
+# # theWholeFile = openDis.read()
+
+# # print(theWholeFile)
+
+# # vacationFile.close()
+
+
+# file =  open("vacationPlaces.txt","r")
+
+# def load(x):
+#     # for i in x:
+#     #     print(i,end="")
+    
+#     out = x.read()
+#     return out
+
+# print(load(file))
+# # load(file)
+
+
+with open("vacationPlaces.txt","r") as vacationFile:
+    for line in vacationFile:
+        print(line,end="")
---
+++ fileIOSecondMethod.py
@@ -0,0 +1,28 @@
+# another way to open a file, the advantage is that you don't have to remember to close the file
+
+# with open("vacationPlaces.txt","r") as txtfile:
+#     for line in txtfile:
+#         print(line,end="")
+
+
+# appending using new method 
+
+
+asian_cities = ['Tokyo', 'Delhi', 'Shanghai', 'Sao Paulo', 'Mumbai', 'Dhaka', 'Mexico City', 'Cairo', 'Bangkok', 'Karachi']
+
+with open("holiday.txt","w") as txtfile:
+    for city in asian_cities:
+        txtfile.write(city+"\n")    
+
+# holiday = open("holiday.txt","r")
+
+# for line in holiday:
+#     print(line,end="")
+
+# holiday.close()    
+
+with open("holiday.txt","r") as txtfile:
+    for line in txtfile:
+        print(line)
+    # out = txtfile.read()  
+    # print(out)  
