5e295a997efa5e19bbf62f6f2994a897a868caca
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-11-15 14:45:14 +0200

    Refactored the code to use a function for drawing the tic-tac-toe board

--- ticktackB.py
+++ ticktackB.py
@@ -1,17 +1,15 @@
-
-even = " | | "
-odd = "_____"
-
-
-for i in range(5):
-    if i%2 == 0:
-        # print(even)
-        for j in range(5):
-            if j%2 ==0:
-                print(" ",end="")
-            else:
-                print("|",end="")
-                
-                
-    else:
-        print(odd)    
+def draw():
+    for i in range(5):
+        if i%2 == 0:
+            # print(even)
+            for j in range(5):
+                if j%2 ==0:
+                    print(" ",end="")
+                else:
+                    print("|",end="")
+            print()        
+                            
+        else:
+            print("_____")    
+            
+draw()            
