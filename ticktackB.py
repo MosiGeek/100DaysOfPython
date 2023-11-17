e2e2e83b0bb6d14c8be9a6704ff2dd65e0d59aa7
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-11-17 17:03:39 +0200

    The commit diff modifies the  function to print the grid in a more readable format, and modifies the  function to check for a winner in all possible directions.

--- ticktackB.py
+++ ticktackB.py
@@ -1,84 +1,69 @@
-# def drawGrid(gkey):
-#     for row in range(5):
-#         if row % 2 == 0:
-#             val_row = int(row/2)
+def drawGrid(gkey):
+    for row in range(5):
+        if row % 2 == 0:
+            val_row = int(row / 2)
 
-#             # print(even)
-#             for col in range(5):
-#                 if col % 2 == 0:
-#                     val_col = int(col/2)
+            # print(even)
+            for col in range(5):
+                if col % 2 == 0:
+                    val_col = int(col / 2)
 
-#                     print(gkey[val_col][val_row], end="")
-#                 else:
-#                     print("|", end="")
-#             print()
+                    print(gkey[val_col][val_row], end="")
+                else:
+                    print("|", end="")
+            print()
 
-#         else:
-#             print("_____")
+        else:
+            print("_____")
+            
 
-
-# Player = 1
-# gridKey = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]  # row by column
-
-# print(gridKey)
-# drawGrid(gridKey)
-
-# while True:
-
-#     print(f"Current Player : {Player}")
-
-#     row = int(input("Enter Row :"))
-#     col = int(input("Enter column :"))
-
-
-#     if gridKey[col][row] == " ":
-
-#         if Player == 1:
-#             gridKey[col][row] = "X"
-#             Player = 2
-#         else:
-
-#             gridKey[col][row] = "O"
-#             Player = 1
-
-#     print(gridKey)
-#     drawGrid(gridKey)
-
-
-# # counter = 0
-# # while counter <= 2:
-# #     for i in gridKey[counter]:
-# #         print(i, end=" ")
-# #     print("\n")
-# #     counter += 1
-
-
-# checking if a player has won
-# check if a certain row or column has the same type e.g x or o
-# what if i use a function that will text the gridKey for every row/column and return True if winner or Draw if there is no winner
-
-gridKey = [["X", "X", "O"], ["O", "X", "X"], ["X", "X", "O"]]
-
-# check
-# check rows
-def check_winner(grid):
+def check_winner(gKey):
+    gridKey = gKey
     if (
         bool(gridKey[0][0] == gridKey[0][1] == gridKey[0][2])
+        and (gridKey[0][0] == gridKey[0][1] == gridKey[0][2] != " ")
         or bool(gridKey[1][0] == gridKey[1][1] == gridKey[1][2])
+        and (gridKey[1][0] == gridKey[1][1] == gridKey[1][2] != " ")
         or bool(gridKey[2][0] == gridKey[2][1] == gridKey[2][2])
+        and (gridKey[2][0] == gridKey[2][1] == gridKey[2][2] != " ")
         or bool(gridKey[0][0] == gridKey[1][0] == gridKey[2][0])
+        and (gridKey[0][0] == gridKey[1][0] == gridKey[2][0] != " ")
         or bool(gridKey[0][1] == gridKey[1][1] == gridKey[2][1])
+        and (gridKey[0][1] == gridKey[1][1] == gridKey[2][1] != " ")
         or bool(gridKey[0][2] == gridKey[1][2] == gridKey[2][2])
+        and (gridKey[0][2] == gridKey[1][2] == gridKey[2][2] != " ")
         or bool(gridKey[2][0] == gridKey[1][1] == gridKey[0][2])
+        and (gridKey[2][0] == gridKey[1][1] == gridKey[0][2] != " ")
         or bool(gridKey[0][1] == gridKey[1][1] == gridKey[2][2])
+        and (gridKey[0][1] == gridKey[1][1] == gridKey[2][2] != " ")
     ):
-        return print("WINNER !!")
-
-check_winner(gridKey)
+        print("Winnnnner !!")
 
 
-# print(gridKey[0][1])
-# print(gridKey[1][1])
-# print(gridKey[1][2])
+Player = 1
+gridKey = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]  # row by column
 
-# print(check)
+# print(gridKey)
+drawGrid(gridKey)
+
+
+
+
+while True:
+    print(f"Current Player : {Player}")
+    # check_winner(gridKey)
+
+    row = int(input("Enter Row :"))
+    col = int(input("Enter column :"))
+
+    if gridKey[col][row] == " ":
+        if Player == 1:
+            gridKey[col][row] = "X"
+            Player = 2
+        else:
+            gridKey[col][row] = "O"
+            Player = 1
+
+    # print(gridKey)
+    drawGrid(gridKey)
+    check_winner(gridKey)
