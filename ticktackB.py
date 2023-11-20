6f4a9e5100c5fe3dcb4d2eb91660a134433bff2b
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2023-11-20 10:39:18 +0200

    added a list of turns to the game, this will keep track of the last player and pop out if he wins

--- ticktackB.py
+++ ticktackB.py
@@ -17,9 +17,9 @@ def drawGrid(gkey):
             print("_____")
             
 # function to check whether the values of the horizontal, vetical, diagonal axis are equal, if equal then return winner  
-def check_winner(gKey,player):
+def check_winner(gKey,turn):
     gridKey = gKey
-    winning_player = str(player)
+    winning_player = str(turn.pop())
     if (
         bool(gridKey[0][0] == gridKey[0][1] == gridKey[0][2])
         and (gridKey[0][0] == gridKey[0][1] == gridKey[0][2] != " ")
@@ -43,6 +43,7 @@ def check_winner(gKey,player):
 
 Player = 1
 gridKey = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]  # row by column
+turn = []
 
 # print(gridKey)
 drawGrid(gridKey)
@@ -58,11 +59,15 @@ while True:
     if gridKey[col][row] == " ":
         if Player == 1:
             gridKey[col][row] = "X"
+            turn.append(Player)
             Player = 2
+            
         else:
             gridKey[col][row] = "O"
+            turn.append(Player)
             Player = 1
 
     # print(gridKey)
     drawGrid(gridKey)
-    check_winner(gridKey,Player)
+    check_winner(gridKey,turn)
+
