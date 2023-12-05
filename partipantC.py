257e9dae7ff275fd26cd8834f2d0f636c4a8f73d
MosiGeek <irvine.mushonga@gmail.com>
2023-12-05 13:30:26 +0200

    revision

--- partipantC.py
+++ partipantC.py
@@ -1,7 +1,28 @@
-# no comment yet
+ParticipantNumber = 3
 
+ParticipantData = []
 
+regParticipants = 0 
 
-# with open("vacationPlaces.txt","r") as doc:
-#     for line in doc:
-#         print(line)
+while (regParticipants < ParticipantNumber):
+    temp = []
+    name = str(input(f"whats your name?:{regParticipants}\n"))
+    temp.append(name)
+    country = str(input(f"Country? :{regParticipants}\n"))
+    temp.append(country)
+    age = str(input(f"How Old are You? :{regParticipants}\n"))
+    temp.append(age)
+    ParticipantData.append(temp)
+    regParticipants += 1
+
+print(ParticipantData) 
+
+for participant in ParticipantData:
+    print(participant)   
+
+with open("girls.txt","w") as data:
+    for participant in ParticipantData:
+        data.write(str(participant))
+        data.write("\n")
+
+    
