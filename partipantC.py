3451f394e81495d6bee73bbed32b098868392d5b
MosiGeek <irvine.mushonga@gmail.com>
2023-12-06 08:23:42 +0200

    reading data

--- partipantC.py
+++ partipantC.py
@@ -1,33 +1,35 @@
-ParticipantNumber = 3
+# ParticipantNumber = 3
 
-ParticipantData = []
+# ParticipantData = []
 
-regParticipants = 0 
+# regParticipants = 0 
 
-while (regParticipants < ParticipantNumber):
-    temp = []
-    name = str(input(f"whats your name?:{regParticipants}\n"))
-    temp.append(name)
-    country = str(input(f"Country? :{regParticipants}\n"))
-    temp.append(country)
-    age = str(input(f"How Old are You? :{regParticipants}\n"))
-    temp.append(age)
-    ParticipantData.append(temp)
-    regParticipants += 1
+# while (regParticipants < ParticipantNumber):
+#     temp = []
+#     name = str(input(f"whats your name?:{regParticipants}\n"))
+#     temp.append(name)
+#     country = str(input(f"Country? :{regParticipants}\n"))
+#     temp.append(country)
+#     age = str(input(f"How Old are You? :{regParticipants}\n"))
+#     temp.append(age)
+#     ParticipantData.append(temp)
+#     regParticipants += 1
 
 # print(ParticipantData) 
 
 # for participant in ParticipantData:
 #     print(participant)   
 
-with open("girls.txt","w") as data:
-    for participant in ParticipantData:
-        for row in participant:
-            data.write(str(row))
-            data.write(" ")
-        data.write("\n")
+# with open("girls.txt","w") as data:
+#     for participant in ParticipantData:
+#         for row in participant:
+#             data.write(str(row))
+#             data.write(" ")
+#         data.write("\n")
 
-# with open("girls.txt","r") as data:
-#     for row in data:
-#         row.strip("\n")
+with open("girls.txt","r") as file:
+    for line in file:
+        print(line)    
+        
+
 
