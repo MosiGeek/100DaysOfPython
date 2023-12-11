0063308f0e55772a386f33f54b6b5ef9d174233b
MosiGeek <irvine.mushonga@gmail.com>
2023-12-11 11:53:28 +0200

    Big Team

--- classes.py
+++ classes.py
@@ -1,9 +1,20 @@
 
-class Cats:
-    def __init__(self):
-        self.Attribute = 0
+# class Cats:
+#     def __init__(self):
+#         self.Attribute = 0
 
-    def AnotherFunction(self):
-        Action(s)    
+#     def AnotherFunction(self):
+#         Action(s)    
         
-    
+class Team:
+    def __init__(self):
+            self.TeamName = "NaN"
+            self.TeamOrigin = "NaN"
+
+
+Team1 = Team()
+
+# Team1.TeamName = "Barcelona"
+
+print(Team1.TeamName)
+print(Team1.TeamOrigin)
