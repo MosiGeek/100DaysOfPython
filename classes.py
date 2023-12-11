db435e9d403bf7f2e150960c3447aac2bac1ff86
MosiGeek <irvine.mushonga@gmail.com>
2023-12-11 12:06:57 +0200

    getters and setters

--- classes.py
+++ classes.py
@@ -11,10 +11,18 @@ class Team:
             self.TeamName = "NaN"
             self.TeamOrigin = "NaN"
 
+    def setTeamName(self,name):
+          self.TeamName = name    
+
+    def setTeamOrigin(self,origin):
+          self.TeamOrigin = origin          
+
 
 Team1 = Team()
 
 # Team1.TeamName = "Barcelona"
+Team1.setTeamName("RealMadrid")
+Team1.setTeamOrigin("Spain")
 
 print(Team1.TeamName)
 print(Team1.TeamOrigin)
