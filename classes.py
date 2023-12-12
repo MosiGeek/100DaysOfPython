5fd5e096b37b4f69552e537e20666309425aa09d
MosiGeek <irvine.mushonga@gmail.com>
2023-12-12 10:40:24 +0200

    more on classes

--- classes.py
+++ classes.py
@@ -7,7 +7,7 @@
 #         Action(s)    
         
 class Team:
-    def __init__(self,Name,Origin):
+    def __init__(self, Name = "Name", Origin = "Origin"):
             self.TeamName = Name
             self.TeamOrigin = Origin
 
@@ -20,6 +20,7 @@ class Team:
 
 Team1 = Team("","")
 Team2 = Team("Orlando Pirates","South Africa")
+Team3 = Team()
 
 # Team1.TeamName = "Barcelona"
 Team1.setTeamName("RealMadrid")
@@ -30,3 +31,20 @@ print(f"Team Origin: {Team1.TeamOrigin}")
 
 print(Team2.TeamName)
 print(Team2.TeamOrigin)
+print(Team3.TeamName)
+print(Team3.TeamOrigin)
+
+class Shapes:
+    def __init__(self,len,heit):
+            self.Length = len
+            self.Height = heit
+
+    def  triange_area(self,b,h):
+       area = 1/2*b*h
+       return area
+    
+
+Triangle = Shapes(4,2)
+
+print(Triangle.triange_area(Triangle.Length,Triangle.Height))
+    
