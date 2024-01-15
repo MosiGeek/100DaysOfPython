0790810a2bbc82df7fa6a94e0e171572adaf4f01
MosiGeek <irvine.mushonga@gmail.com>
2024-01-15 13:47:11 +0200

    x

---
+++ fruity.py
@@ -0,0 +1,2 @@
+fruit = input("Enter your favourite fruit and press enter :")
+print(f"your favourite fruit is {fruit}")
--- shapes.py
+++ shapes.py
@@ -1,12 +1,18 @@
 class Shapes:
-    def __init__(self,len,heit):  #more of a constructor
+        
+    def __init__(self,len,heit,rad):  #more of a constructor
             self.Length = len
             self.Height = heit
+            self.Radias = rad
+            self.PI = 3.14
 
     def  triange_area(self,b,h):
        area = 1/2*b*h
        return area
     
+    def circle_area(self,r):
+         area = self.PI*r*r
+         return area
 
 Triangle = Shapes(4,2)
 
