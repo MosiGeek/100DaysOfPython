05c491a9c6b8fbb0d2b61eb170b93e1de46e00ba
MosiGeek <irvine.mushonga@gmail.com>
2024-02-02 15:35:40 +0200

    x

--- inheritence.py
+++ inheritence.py
@@ -5,8 +5,20 @@ class Shapes:
 
     def triangle_area(self):
         return print(1/2*self.base*self.height,"sqr_cm")
+    
+    def rect_area(self):
+        return print(self.base * self.height,"sqr_cm")
+    
+    # printing a custom message 
+    def __str__(self):  
+        return "The base and height are "+str(self.base)+" & "+str(self.height)
         
     
 Trian1 = Shapes(6,2)    
+Rect1 = Shapes(5,8)
 
 Trian1.triangle_area()
+Rect1.rect_area()
+
+
+print(Trian1)
