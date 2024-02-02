6f0d70f8dcfc647a465dde975377cdbb43779fa9
MosiGeek <irvine.mushonga@gmail.com>
2024-02-02 11:53:19 +0200

    x

---
+++ inheritence.py
@@ -0,0 +1,12 @@
+class Shapes:
+    def __init__(self,base=0,height=0):
+        self.base = base
+        self.height = height
+
+    def triangle_area(self,b,h):
+        self.area = 1/2*b*h
+        return print(self.area)
+    
+Trian1 = Shapes(6,2)    
+
+Trian1.triangle_area(Trian1.base,Trian1.height)
