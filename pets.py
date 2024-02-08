83eff4db2c705cfc033e571078d566fcc3fd76bb
MosiGeek <138681949+MosiGeek@users.noreply.github.com>
2024-02-08 11:42:36 +0000

    x

--- pets.py
+++ pets.py
@@ -1,7 +1,20 @@
 class Pet():
-    def __init__(self,name,age,hunger_level,playful):
+    def __init__(self,name="NaN",age=0,hunger_level="NaN",playful=False):
         self.name = name
         self.age = age
         self.hunger_level = hunger_level
         self.playful = playful
+
+    def set_pet_name(self,n):
+        self.name = n    
+
+    def get_pet_name(self):
+        return print(self.name)   
+
+
+Cat = Pet()    
+
+Cat.get_pet_name()
+Cat.set_pet_name("Nija")
+Cat.get_pet_name()
 
