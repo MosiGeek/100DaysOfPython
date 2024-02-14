64c043c0428edfb6fef842cc1e2bfafd400e7871
MosiGeek <irvine.mushonga@gmail.com>
2024-02-14 16:27:30 +0200

    importing in python

---
+++ im.py
@@ -0,0 +1,13 @@
+# importing in python 
+
+import random
+
+# # random.seed(50)
+# dice = random.randint(1,6)
+# print(dice)
+# # if dice == 6:
+# #     roll()
+
+print(round(random.random(),2))
+
+print(random.uniform(1,6))
