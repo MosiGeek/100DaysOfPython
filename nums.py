2f99706f3964b7a96678ec79787396f47a133ed8
MosiGeek <irvine.mushonga@gmail.com>
2024-04-10 12:40:39 +0200

    x

--- nums.py
+++ nums.py
@@ -1,5 +1,13 @@
-miles = float(input("enter the number of miles :"))
+# miles = float(input("enter the number of miles :"))
 
-km = round((miles * 1.6),2)
+# km = round((miles * 1.6),2)
 
-print(f"{km} km")
+# print(f"{km} km")
+
+import tkinter as tk
+from tkinter import ttk
+
+#window
+window = tk.Tk()
+
+window.mainloop()
